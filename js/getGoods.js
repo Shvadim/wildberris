2726f10611e51793ca8adacbab146e4f51e0a6fe
Shvadim <shvadst@gmail.com>
2023-12-01 01:30:23 +0300

    lesson 3 	modified:   goods.html 	modified:   js/getGoods.js 	modified:   js/search.js

--- js/getGoods.js
+++ js/getGoods.js
@@ -1,33 +1,70 @@
 const getGoods = () => {
   const links = document.querySelectorAll(".navigation-link");
-  const getData = () => {
+
+  // Функция отрисовки товаров (с фильтрацией)
+  const renderGoods = (goods) => {
+    const goodsContainer = document.querySelector(".long-goods-list");
+    goodsContainer.innerHTML = "";
+
+    goods.forEach((good) => {
+      const goodBlock = document.createElement("div");
+      goodBlock.classList.add("col-lg-3");
+      goodBlock.classList.add("col-sm-6");
+
+      goodBlock.innerHTML = `
+      <div class="goods-card">
+        <span class="label ${good.label ? null : "d-none"}">${good.label}</span>
+        <img src="db/${good.img}" alt="image: ${good.name}" class="goods-image">
+        <h3 class="goods-title">${good.name}</h3>
+        <p class="goods-description">${good.description}</p>
+        <button class="button goods-card-btn add-to-cart" data-id="${good.id}">
+          <span class="button-price">$${good.price}</span>
+        </button>
+      </div>
+      `;
+      goodsContainer.append(goodBlock);
+    });
+  };
+  // Функция получения списка товаров с сервера
+  const getData = (value, category) => {
     fetch(
       "https://wberri-default-rtdb.europe-west1.firebasedatabase.app/db.json"
     )
       .then((res) => res.json())
       .then((data) => {
-        console.log(data);
-        localStorage.setItem("data", JSON.stringify(data));
-        console.log(localStorage);
+        let array = category
+          ? data.filter((item) => item[category] === value)
+          : data;
+        localStorage.setItem("goods", JSON.stringify(array));
+        if (window.location.pathname !== "/goods.html") {
+          window.location.href = "/goods.html";
+        } else {
+          renderGoods(array);
+        }
       });
   };
 
   links.forEach((link) => {
     link.addEventListener("click", (event) => {
       event.preventDefault();
-      getData();
+      const linkValue = link.textContent;
+      const category = link.dataset.field;
+      getData(linkValue, category);
     });
   });
 
-  // Работа с localStorage
-  /*
+  if (
+    localStorage.getItem("goods") &&
+    window.location.pathname === "/goods.html"
+  ) {
+    renderGoods(JSON.parse(localStorage.getItem("goods")));
+  }
+
+  /* //Работа с localStorage
   localStorage.setItem("goods", JSON.stringify([1, 2, 23]));
-  let goods = JSON.parse(localStorage.getItem("goods"));
-  console.log(localStorage);
-  localStorage.removeItem("goods");
-  console.log(localStorage);
+  let goods = JSON.parse(localStorage.getItem("goods"));  
+  localStorage.removeItem("goods");  
   */
-  // ---------------------
 };
 
 getGoods();
--- js/search.js
+++ js/search.js
@@ -1,15 +1,71 @@
 const search = function () {
   const inputText = document.querySelector(".search-block > input");
   const btnSearch = document.querySelector(".search-block > button");
+  // Получение ссылки View All
+  const linkViewAll = document.querySelector(".more");
 
-  inputText.addEventListener("input", (event) => {
-    // Выводит все содержимое объекта ввода
-    console.log(event.target.value);
-  });
+  // Функция отрисовки товаров (с фильтрацией)
+  const renderGoods = (goods) => {
+    const goodsContainer = document.querySelector(".long-goods-list");
+    goodsContainer.innerHTML = "";
 
-  btnSearch.addEventListener("click", () => {
-    console.log(inputText.value);
-  });
+    goods.forEach((good) => {
+      const goodBlock = document.createElement("div");
+      goodBlock.classList.add("col-lg-3");
+      goodBlock.classList.add("col-sm-6");
+
+      goodBlock.innerHTML = `
+      <div class="goods-card">
+        <span class="label ${good.label ? null : "d-none"}">${good.label}</span>
+        <img src="db/${good.img}" alt="image: ${good.name}" class="goods-image">
+        <h3 class="goods-title">${good.name}</h3>
+        <p class="goods-description">${good.description}</p>
+        <button class="button goods-card-btn add-to-cart" data-id="${good.id}">
+          <span class="button-price">$${good.price}</span>
+        </button>
+      </div>
+      `;
+      goodsContainer.append(goodBlock);
+    });
+  };
+
+  // Функция получения списка товаров с сервера
+  const getData = (value) => {
+    fetch(
+      "https://wberri-default-rtdb.europe-west1.firebasedatabase.app/db.json"
+    )
+      .then((res) => res.json())
+      .then((data) => {
+        let array = data.filter((good) =>
+          good.name.toLowerCase().includes(value.toLowerCase())
+        );
+
+        localStorage.setItem("goods", JSON.stringify(array));
+
+        if (window.location.pathname !== "/goods.html") {
+          window.location.href = "/goods.html";
+        } else {
+          renderGoods(array);
+        }
+      });
+  };
+
+  try {
+    btnSearch.addEventListener("click", () => {
+      getData(inputText.value);
+    });
+  } catch (e) {
+    console.error(e.message);
+  }
+
+  try {
+    linkViewAll.addEventListener("click", (event) => {
+      event.preventDefault();
+      getData("");
+    });
+  } catch (e) {
+    //console.error(e.message);
+  }
 };
 
 search();
